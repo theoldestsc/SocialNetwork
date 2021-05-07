74b413e64df924bd11685f9fdae049477068f8e9
andrew <ferrarigto733@gmail.com>
2021-05-07 14:23:29 +0300

    First commit

---
+++ account/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Profile
+# Register your models here.
+
+@admin.register(Profile)
+class ProfilAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date_of_birth', 'photo')
